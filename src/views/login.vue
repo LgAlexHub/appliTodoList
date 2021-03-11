335b4bd79e1795936b4cdc839af799c8c3e92407
quent <quentincarre2@gmail.com>
2021-03-11 16:09:47 +0100

    update :
    - modif login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,23 @@
 <template>
     <div>
-        
+        <form>
+          <label for="login">Identifiant : <input type="text" id="login" v-model="login"></label><br>
+          <label for="password">Mot de passe : <input type="password" id="password" :v-model="password"></label><br>
+          <button>Connection</button>
+        </form>
     </div>
 </template>
 <script>
+//import { mapActions, mapGetters} from "vuex";
 export default {
-
+  data(){
+    return {
+      login: "",
+      password: ""
+    }
+  },
+  methods:{
+    //...mapActions("",[""]),
+  }
 }
 </script>
